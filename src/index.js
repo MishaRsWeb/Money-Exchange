e1421be66db86567b6fdc115b61237c7bf681d89
michael.rasskazov <misharasskazov1@gmail.com>
2018-09-30 14:24:47 +0400

    makeExchange

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
 let error = {error: "You are rich, my friend! We don't have so much coins for exchange"};
-let empty = "{hf}";
-if (currency > 0) return (error);
-else if (currency = 0) return (empty);
-razmen = {"H":a,"Q":b,"D":c,"N":d,"P":e};
-let obj = currency % 50;
-razmen.H = (currency - obj)/50;
-return razmen.H;
+let empty = {};
+if (currency > 10000) {
+	return (error);
+} else if (currency === 0) {
+	return (empty);
+}
+let exchange = {};
+let obj = (currency - (currency % 50))/50;
+debugger;
+if (obj > 0) {
+	exchange.H = obj;	
+}
+let rest = currency - (obj*50);
+obj = (rest - (rest % 25))/25;
+if (obj > 0) {
+	exchange.Q = obj;	
+}
+rest = rest - (obj*25);
+obj = (rest - (rest % 10))/10;
+if (obj > 0) {
+	exchange.D = obj;	
+}
+rest = rest - (obj*10);
+obj = (rest - (rest % 5))/5;
+if (obj > 0) {
+	exchange.N = obj;	
+}
+rest = rest - (obj*5);
+obj = (rest - (rest % 1))/1;
+if (obj > 0) {
+	exchange.P = obj;	
+}
+return exchange;
 }
